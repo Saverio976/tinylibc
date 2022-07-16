6e116ec4468bf4eb0a509ca763f28024be4268f5
Xavier Mitault <xavier.mitault@epitech.eu>
2022-07-16 15:55:55 +0200

    Add first implementation of stdlib part

---
+++ includes/tlcstdlibs.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** (my) Tiny Lib C
+** File description:
+** prototypes of stdlibs part
+*/
+
+#ifndef TLS_STDLIBS_H_
+
+/**
+** @brief malloc n char and fill with 0
+**
+** @param n number to alloc
+**
+** @return ptr to the first n allocated
+**/
+char *x_calloc(int n);
+
+/**
+** @brief malloc n char * and fill with 0
+**
+** @param n number to alloc
+**
+** @return ptr to the first n allocated
+**/
+char **x_ccalloc(int n);
+
+#endif
